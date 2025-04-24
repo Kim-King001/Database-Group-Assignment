b8fd9a4aa62dc184614e6f6696addc11f6877e5e
KimberlyAchieng <kimberly.achieng.work@gmail.com>
2025-04-24 23:24:29 +0300

    Update size_option.sql

--- size_option.sql
+++ size_option.sql
@@ -1,5 +1,5 @@
 --  Size Option Table
-CREATE TABLE Size_Option (
+CREATE TABLE size_option (
     size_option_id INT AUTO_INCREMENT PRIMARY KEY,
     size_category_id INT NOT NULL,
     size_value VARCHAR(50) NOT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE Size_Option (
 
 
 -- Insert Size Options
-INSERT INTO Size_Option (size_category_id, size_value) VALUES
+INSERT INTO size_option (size_category_id, size_value) VALUES
 (1, '42'),
 (1, '43'),
 (2, 'M'),
