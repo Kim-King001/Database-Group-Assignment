7b67cf959f9f69ebb3ee61d367d48fe75af7c68b
KimberlyAchieng <kimberly.achieng.work@gmail.com>
2025-04-24 23:30:02 +0300

    Update colour.sql

--- colour.sql
+++ colour.sql
@@ -1,12 +1,12 @@
 -- Colour Table
-CREATE TABLE Colour (
+CREATE TABLE colour (
     colour_id INT AUTO_INCREMENT PRIMARY KEY,
     colour_name VARCHAR(50) NOT NULL,
     hex_code VARCHAR(7) -- e.g., #FFFFFF
 );
 
 -- Insert Colours
-INSERT INTO Colour (colour_name, hex_code) VALUES
+INSERT INTO colour (colour_name, hex_code) VALUES
 ('Black', '#000000'),
 ('White', '#FFFFFF'),
 ('Blue', '#0000FF');
