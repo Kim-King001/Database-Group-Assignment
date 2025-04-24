ba2d0e46cef85d75d24c841ac4e5ccc09a4ef26c
KimberlyAchieng <kimberly.achieng.work@gmail.com>
2025-04-24 23:27:03 +0300

    Update size_category.sql

--- size_category.sql
+++ size_category.sql
@@ -1,10 +1,10 @@
 -- Size Category Table
-CREATE TABLE Size_Category (
+CREATE TABLE size_category (
     size_category_id INT AUTO_INCREMENT PRIMARY KEY,
     size_category_name VARCHAR(100) NOT NULL
 );
 
 -- Insert Size Categories
-INSERT INTO Size_Category (size_category_name) VALUES
+INSERT INTO size_category (size_category_name) VALUES
 ('Men Shoe Sizes'),
 ('Men Clothing Sizes');
